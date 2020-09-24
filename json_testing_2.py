f8520f00b7a98607762afe6fccc432684bba1e6a
NezzyG <71039838+NezzyG@users.noreply.github.com>
2020-09-24 15:09:47 +0300

    Update json_testing_2.py

--- json_testing_2.py
+++ json_testing_2.py
@@ -1,27 +1,10 @@
-"""oricum e irelevant, o sa iti scriu aici ce idee am sa faci in continuare
-vreau sa te joci cu libraria "requests": vreau sa faci request catre pagina asta https://api.mocki.io/v1/b043df5a
-response-ul va fi un json (daca intri pe link vei vedea)
-vreau sa imi afisezi toate orasele alfabetic
-un tiny exercise pt a te invata si cu request-urile catre pagini
-si are legatura cu JSON
-si la interviul de la voda am avut de facut un request de genul"""
-
-
-
-
-
+import requests
 import json
-
 from pprint import pprint
-
-
-
-with open("users.json","w",encoding="utf-8") as f:
-    data = json.dumps(steam, indent=4)
-    f.write(data)
-with open("users.json","r",encoding="utf-8") as f:
-    data=json.load(f)
-    pprint(data)
-with open("users.json","w", encoding="utf-8") as f:
-    data=json.dumps(data, indent=4)
-    f.write(data)
+data= requests.get('https://api.mocki.io/v1/b043df5a')
+data= json.loads(data.text)
+pprint(data)
+myList=[]
+for n in range(len(data)):
+ myList.append(data[n]['city'])
+print(sorted(myList))
